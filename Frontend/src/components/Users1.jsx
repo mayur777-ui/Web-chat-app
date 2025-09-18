4c853315e8db39727c30604443b1e9a1cafd119d
Mayur Lakshkar <mayurlaxkar76@gmail.com>
2025-09-18 14:47:43 +0530

    Update Users1.jsx

--- Frontend/src/components/Users1.jsx
+++ Frontend/src/components/Users1.jsx
@@ -19,8 +19,8 @@ import { motion } from "framer-motion";
 import { MessageSquareText } from "lucide-react";
 
 export default function Users1() {
-  const USER_API_END_POINT = "http://localhost:5000/user";
-  const Notification_API_END_POINT = "http://localhost:5000/notification";
+  const USER_API_END_POINT = 'https://webchat-backend-658o.onrender.com/user';
+  const Notification_API_END_POINT = 'https://webchat-backend-658o.onrender.com/notification';
   const token = localStorage.getItem("token");
   const [loggedInUser, setLoggedInUser] = useState({});
   const [connectionList, setConnectionList] = useState([]);
